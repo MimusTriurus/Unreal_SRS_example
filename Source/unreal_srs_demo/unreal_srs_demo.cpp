d039042021aa65444c602738c51020385b069bf9
Zvyagincev <a.v.zvyagincev@yandex.ru>
2023-03-19 11:41:55 +0300

    add c++ class for the "generate solution" feature

---
+++ Source/unreal_srs_demo/unreal_srs_demo.cpp
@@ -0,0 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "unreal_srs_demo.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, unreal_srs_demo, "unreal_srs_demo" );
